672e0020deecbab1b3728f206d18ed7d3f433f5f
VitalijVictor <vitalijvictor@gmail.com>
2021-05-20 20:15:10 +0200

    Jukebox
    version2

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -1,19 +1,20 @@
 package ru.job4j.oop;
 
 public class Jukebox {
-    public static void music(int position){
+    public void music(int position){
         if (position == 1){
             System.out.println("Пусть бегут неуклюже");
         }
         if (position == 2){
             System.out.println("Спокойной ночи");
         }
-        else {
+        if (position < 1 || position > 2) {
             System.out.println("Песня не найдена");
         }
     }
 
-    public static void main(String[] args) {
-        Jukebox.music(1);
+        public static void main(String[] args) {
+        Jukebox jukebox = new Jukebox();
+        jukebox.music(0);
     }
 }
