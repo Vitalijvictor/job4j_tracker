f9e7dd8fe9c1df245d399c028873a091043b4bd9
VitalijVictor <vitalijvictor@gmail.com>
2021-05-21 19:35:40 +0200

    DummyDic
    version3

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,7 +2,6 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String translate =  "Неизвестное слово: " + eng;
         return "Неизвестное слово: " + eng;
     }
 }
