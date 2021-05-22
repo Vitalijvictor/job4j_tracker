cccfed654a7e99d2740233c401ef9022e312f508
VitalijVictor <vitalijvictor@gmail.com>
2021-05-22 13:43:20 +0200

    DummyDic
    version4

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -4,4 +4,10 @@ public class DummyDic {
     public String engToRus(String eng) {
         return "Неизвестное слово: " + eng;
     }
+
+    public static void main(String[] args) {
+        DummyDic word = new DummyDic();
+        System.out.println(word.engToRus("yhjfg"));
+
+    }
 }
