efccb8c2f6a7cab8da2f2c591b076a6ad5f846bd
VitalijVictor <vitalijvictor@gmail.com>
2021-05-22 13:48:45 +0200

    DummyDic
    version5

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -1,20 +1,25 @@
 package ru.job4j.oop;
+/*
+@ if
+@ else if
+@ else
+*/
 
 public class Jukebox {
     public void music(int position){
         if (position == 1){
             System.out.println("Пусть бегут неуклюже");
         }
-        if (position == 2){
+        else if (position == 2){
             System.out.println("Спокойной ночи");
         }
-        if (position < 1 || position > 2) {
+        else {
             System.out.println("Песня не найдена");
         }
     }
 
         public static void main(String[] args) {
         Jukebox jukebox = new Jukebox();
-        jukebox.music(0);
+        jukebox.music(1);
     }
 }
