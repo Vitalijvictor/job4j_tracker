6a39ac97827bf978b1300a962211d41d6c6d9d39
VitalijVictor <vitalijvictor@gmail.com>
2021-05-23 17:38:02 +0200

    Triangle
    version1

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return -1;
+    }
+
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+
+                p = (ab + ac + bc) / 2;
+                rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+                return rsl;
+        }
+        return rsl;
+    }
+}
