97563313ca5a938e065a71bd3b0f0f042b11cbae
VitalijVictor <vitalijvictor@gmail.com>
2021-05-23 09:18:25 +0200

    Battery
    version1

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,20 @@
+/*
+1. Создать класс ru.job4j.oop.Battery. в нем должно быть поле private int load.
+2. Создать метод public void exchange(Battery another). Этот метод должен списывать заряд из батареи у кого вызывали метод exchange и добавить к объекту another.
+ */
+
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int charge){
+        this.load = charge;
+    }
+
+    public void exchange(Battery another){
+        this.load = this.load - another.load;
+        another.load = 0;
+    }
+
+}
