63e10e5602c70f6ea0fbb6c146c4e6ea3859ca76
VitalijVictor <vitalijvictor@gmail.com>
2021-05-26 18:48:33 +0200

    Triangle
    18:47 26/05/2021

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -24,10 +24,9 @@ public class Triangle {
         double ab = first.distance(second);
         double ac = first.distance(third);
         double bc = second.distance(third);
-        double p = period(ab, ac, bc);
 
         if (this.exist(ab, ac, bc)) {
-                p = p = period(ab, ac, bc);
+                double p = period(ab, ac, bc);
                 rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
                 return rsl;
         }
