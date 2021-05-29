8cd98436f2de15efcace86a3a63719ab2f154ceb
VitalijVictor <vitalijvictor@gmail.com>
2021-05-29 22:59:52 +0200

    MagicBall
    version1

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String name = input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("yes");
+        }
+        if (answer == 1) {
+            System.out.println("no");
+        }
+        else {
+            System.out.println("Maybe");
+        }
+    }
+}
