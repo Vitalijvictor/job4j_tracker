64b0f9d1468ac1385dab115442d05d4f97d0ab2c
VitalijVictor <vitalijvictor@gmail.com>
2021-05-31 12:08:42 +0200

    Tracker
    Version1

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,27 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll(){
+        Item[] itemTmp = Arrays.copyOf(items, size);
+        return itemTmp;
+    }
+
+    /*public Item findByName(String key){
+        Item items = Arrays.copyOf(name.getName(key), size);
+    }*/
+
+    public Item findByName(String key){
+        Item rsl = null;
+        for (int index = 0; index < size; index++){
+            Item item = items[index];
+            if (item.getName() == key){
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
     }
 
     public Item findById(int id) {
