8e9d0aada024e74ecc26a218e2026e6f283a18a5
VitalijVictor <vitalijvictor@gmail.com>
2021-06-01 14:14:14 +0200

    FactTest
    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static ru.job4j.ex.Fact.calc;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calcFact5To120() {
+        int rsl  = 120;
+        rsl = Fact.calc(5);
+    }
+}
