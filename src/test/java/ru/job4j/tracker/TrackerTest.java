96f4a4656c19abab5c04b22c947deb1d39653d11
VitalijVictor <vitalijvictor@gmail.com>
2021-06-01 18:37:38 +0200

    TrackerTest
    void fix delete

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,8 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class TrackerTest {
     @Test
@@ -32,7 +31,49 @@ public class TrackerTest {
         assertEquals(2, allItems.length);
         assertEquals("test1", allItems[0].getName());
 
+        assertEquals(1, allItems[0].getId());
+        assertEquals(2, allItems[1].getId());
+
 //        for (Item i : allItems)
 //            System.out.println(Integer.valueOf(i.getId()) + " : " + i.getName());
     }
+
+    @Test
+    public void testIndexOfExisting() {
+        Tracker tracker = new Tracker();
+
+        Item item = new Item();
+        item.setName("test1");
+        tracker.add(item);
+
+        assertEquals(item, tracker.findById(1) );
+        assertNull(tracker.findById(2));
+
+    }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
+
+    @Test
+    public void whenDelete() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        tracker.delete(id);
+        //assertThat(tracker.findById(id), is(nullValue()));
+        assertNull(tracker.findById(id));
+    }
+
 }
