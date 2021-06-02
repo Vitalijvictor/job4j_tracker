fe1bbb397c0aa47edddc793181811aafc69369c7
VitalijVictor <vitalijvictor@gmail.com>
2021-06-02 11:35:52 +0200

    calcFact5To120 --> calcFactMinus1To120

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -8,7 +8,7 @@ import static ru.job4j.ex.Fact.calc;
 public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void calcFact5To120() {
+    public void calcFactMinus1To120() {
         int rsl  = 120;
         rsl = Fact.calc(-1);
     }
