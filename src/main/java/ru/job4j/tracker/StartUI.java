face309745aa980759c7cddf6804dd37a3b95fd7
VitalijVictor <vitalijvictor@gmail.com>
2021-06-10 20:57:43 +0200

    9.1 Зависимость от System.out [#33568 #166172]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -34,11 +34,11 @@ public class StartUI {
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output),
-                new ShowAllAction(),
-                new ReplaceAction(),
-                new DeleteAction(),
-                new FindByIdAction(),
-                new FindByNameAction(),
+                new ShowAllAction(output),
+                new ReplaceAction(output),
+                new DeleteAction(output),
+                new FindByIdAction(output),
+                new FindByNameAction(output),
                 new ExitAction()
         };
         new StartUI(output).init(input, tracker, actions);
