d8600fa701c076f6a61f88c70bde3d84042cad34
VitalijVictor <vitalijvictor@gmail.com>
2021-06-15 12:33:44 +0200

    ValidateInputTest
    2.1. Тест на ValidateInput [#311887 #167514]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -48,6 +48,6 @@ public class ValidateInputTest {
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
-        assertThat(selected, is("Please enter validate data again."));
+        assertThat(selected, is(-2));
     }
 }
