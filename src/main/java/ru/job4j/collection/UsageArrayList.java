69beccc3cc929d2d858d3c04607b5ac5750aaf95
VitalijVictor <vitalijvictor@gmail.com>
2021-06-20 16:20:21 +0200

    0. Коллекции, ArrayList, List, Обобщения [#10094 #169511]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
-        names.add("Petr, Ivan, Stepan");
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
         for (Object value : names) {
             System.out.println(value);
         }
