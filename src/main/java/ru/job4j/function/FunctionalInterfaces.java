0d8631e75c436b4d1bd285c36346ae7e49640b39
VitalijVictor <vitalijvictor@gmail.com>
2021-07-06 15:55:20 +0200

    1.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -1,46 +1,28 @@
 package ru.job4j.function;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.*;
 
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biCon =
-                (s, s1) -> System.out.println(s + s1);
-        biCon.accept(map.get(0), map.get());
-        /*
-            Замените вставку элементов в карту на использование BiConsumer, он уже объявлен, требуется его реализовать.
-         */
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-
-        BiPredicate<Integer, String> biPred =
-                (i, s) -> s.contains(i.toString());
+        BiPredicate<Integer, String> biPred = (integer, s) -> integer % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
-            /*
-                Замените проверку в if() на использование BiPredicate, он объявлен выше, требуется его реализовать.
-             */
-
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
         }
         /*
             Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
          */
-        Supplier<List<String>> sup = () -> new HashMap<Supplier>(map);
+        Supplier<List<String>> sup = () -> new HashSet<>(map);
         List<String> strings = new ArrayList<>(map.values());
 
-        Consumer<String> con = (Consumer<String>) sup.get();
+        Consumer<String> con = con.accept(sup.get());
         Function<String, String> func = (s, s1) ->
-                con.accept(sup.get());
+                con.accept(func.get());
         for (String s : strings) {
+            Consumer<String> consumer = (s) -> System.out.println(s);
             /*
                 Заменить вывод строк на применение Consumer
                 Заменить преобразование строк к строкам в верхнем регистре с помощью Function
