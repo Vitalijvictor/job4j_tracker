428f809c8dd247ac344975f1bb0a508d6fb32a14
VitalijVictor <vitalijvictor@gmail.com>
2021-07-07 14:35:44 +0200

    1.3. Встроенные функциональные интерфейсы. [#249209 #174859]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -6,35 +6,12 @@ import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class SearchFolder {
-    public static List<Folder> filterSize(List<Folder> list) {
-        List<Folder> rsl = new ArrayList<>();
-        for (Folder f : list) {
-            if (f.getSize() > 100) {
-                rsl.add(f);
-            }
-        }
-        return rsl;
-    }
-
-    public static List<Folder> filterName(List<Folder> list) {
-        List<Folder> rsl = new ArrayList<>();
-        for (Folder f : list) {
-            if (f.getName().contains("bug")) {
-                rsl.add(f);
-            }
-        }
-        return rsl;
-    }
-
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         List<Folder> rsl = new ArrayList<>();
-        BiPredicate<Folder, Integer> predicate =
-                (s, i) -> rsl.contains("bug");
         for (Folder f : list) {
-            if (predicate.test(f, rsl.size())) {
+            if (pred.test(f)) {
                 rsl.add(f);
             }
-        }
-        return rsl;
+        } return rsl;
     }
 }
