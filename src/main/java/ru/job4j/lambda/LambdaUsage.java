ae4014b24996f31caf4218340849de7ddda7065f
VitalijVictor <vitalijvictor@gmail.com>
2021-07-08 15:38:54 +0200

    2.2. Лямбда блок [#249208] & 2.5. Ленивая загрузка [#249211 #175466]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,5 +1,6 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
@@ -8,5 +9,15 @@ public class LambdaUsage {
             System.out.println("compareDes - " + right.length() + " and " + left.length());
             return Integer.compare(right.length(), left.length());
         };
+
+        String[] names = {
+                "Ivan",
+                "Petr"
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return left.length() - right.length();
+        };
+        Arrays.sort(names, lengthCmp);
     }
 }
