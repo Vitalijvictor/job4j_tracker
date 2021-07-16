eaff106657e5854d3acc4c0dda3ee7453fd2801a
VitalijVictor <vitalijvictor@gmail.com>
2021-07-16 19:47:46 +0200

    3. Уникальность элементов и сортировка. [#110225 #178092]

--- src/main/java/ru/job4j/lambda/Profiles.java
+++ src/main/java/ru/job4j/lambda/Profiles.java
@@ -1,21 +1,19 @@
 package ru.job4j.lambda;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(x -> x.getAddress())
-                .sorted(
-                    new Comparator<Address>() {
-                        public int compare(Address city1, Address city2) {
-                            return city1.getCity().compareTo(city2.getCity());
-                        }
-                    }
+                .sorted(Comparator.comparing(Address::getCity)
                 )
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
