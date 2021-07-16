e1caf6cc0a12ba56c1a51684801ba3c3534e1815
VitalijVictor <vitalijvictor@gmail.com>
2021-07-16 19:28:27 +0200

    2. Список адресов [#110062 #178064]
    тест

--- src/test/java/ru/job4j/lambda/ProfilesTest.java
+++ src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -2,19 +2,31 @@ package ru.job4j.lambda;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
 
     @Test
-    public void collectToList() {
+    public void sortedToList() {
         List<Profile> profiles = List.of(
                 new Profile("Johan", "Antwerpen", "Statiestraat", 10, 5),
                 new Profile("Dirk", "Deurne", "Mollenstraat", 8, 3),
                 new Profile("Lola", "Brussel", "Ryckenstraat", 1, 2),
                 new Profile("Molly", "Gent", "Bruggestraat", 5, 4)
         );
-        profiles.stream().map(x -> x.getAddress()).collect(Collectors.toList());
+        List<Address> expectedAddresses = Arrays.asList(
+                new Address("Antwerpen", "Statiestraat", 10, 5),
+                new Address("Brussel", "Ryckenstraat", 1, 2),
+                new Address("Deurne", "Mollenstraat", 8, 3),
+                new Address("Gent", "Bruggestraat", 5, 4)
+        );
+
+        List<Address> addresses = (new Profiles()).collect(profiles);
+        assertThat(addresses, is(expectedAddresses));
     }
 }
