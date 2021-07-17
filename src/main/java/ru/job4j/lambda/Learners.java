543615e3f6b11c4b7fa348e01ba87ef893fe58c6
VitalijVictor <vitalijvictor@gmail.com>
2021-07-17 11:03:48 +0200

    4. Преобразование List в Map. [#110226]
    Список без теста

--- src/main/java/ru/job4j/lambda/Learners.java
+++ src/main/java/ru/job4j/lambda/Learners.java
@@ -8,20 +8,17 @@ import java.util.stream.Collectors;
 public class Learners {
     public static void main(String[] args) {
         List<Student> students = List.of(
-                new Student(30, "Biily"),
+                new Student(30, "Billy"),
                 new Student(20, "Willy"),
                 new Student(70, "Ray"),
                 new Student(60, "John"),
                 new Student(90, "Jeniffer")
         );
 
-        public void tomap(Learners students) {
-            System.out.println(
-                    List.of(students.stream().distinct().collect(Collectors.toMap(
-                            e -> students.getSurname(),
-                            e -> students.getScore()
-                            )
-                    )));
+        students.stream().distinct().collect(Collectors.toMap(
+                e -> e.getSurname(),
+                e -> e.getScore()
+        ));
         }
-    }
 }
+
