07ccec7823737a7d497a1f8ce8a11c7646cdc021
VitalijVictor <vitalijvictor@gmail.com>
2021-08-14 16:22:59 +0200

    2. Отсортировать департаменты [#34352 #188555]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,10 +1,19 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return 0;
+        List<String> d1 = Arrays.asList(o1.split("/"));
+        List<String> d2 = Arrays.asList(o2.split("/"));
+
+        if (d1.get(0).equals(d2.get(0))) {
+            return o1.compareTo(o2);
+        } else {
+            return o2.compareTo(o1);
+        }
     }
 }
--- src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 
 public class DepDescCompTest {
 
@@ -15,6 +16,18 @@ public class DepDescCompTest {
                 "K2/SK1/SSK1"
         );
         assertThat(rsl, greaterThan(0));
+
+        rsl = new DepDescComp().compare(
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2"
+        );
+        assertThat(rsl, lessThan(0));
+
+        rsl = new DepDescComp().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK2"
+        );
+        assertThat(rsl, is(0));
     }
 
     @Test
