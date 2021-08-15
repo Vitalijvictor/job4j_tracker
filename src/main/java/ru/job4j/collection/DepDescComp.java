3a345b6b23e3d5e3439d6dadfa30ad435a71eddb
VitalijVictor <vitalijvictor@gmail.com>
2021-08-15 10:54:12 +0200

    4. Optional в Stream API. [#279130 #181489]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,14 +7,6 @@ import java.util.List;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-//        List<String> d1 = Arrays.asList(o1.split("/"));
-//        List<String> d2 = Arrays.asList(o2.split("/"));
-//
-//        if (d1.get(0).equals(d2.get(0))) {
-//            return o1.compareTo(o2);
-//        } else {
-//            return o2.compareTo(o1);
-//        }
         String[] o1Split = o1.split("/");
         String[] o2Split = o2.split("/");
         int rsl = o2Split[0].compareTo(o1Split[0]);
